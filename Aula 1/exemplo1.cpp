c90f680a6b6b5fcf0c63ca63a443d420b758e628
Arthur Vinicius Barreto Demetrio <barreto.arthur@academico.ifrn.edu.br>
2026-08-14 13:19:00 +0000

    adiciona um arquivo em C#

--- Aula 1/exemplo1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-//using namespace std
-    int main(){
-        std::string name;
-        std::cout << "what's your name?";
-        std::cin >> name;
-        std::cout << "hello " << name;
-        return 0;
-}
--- Aula 1/exemplo1.py
+++
@@ -1,2 +0,0 @@
-nome = input("digite seu nome:")
-print(nome, "says: hello world!")
